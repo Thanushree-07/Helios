84f752511a11fd2bbb9796deeb9fe3829ea29d4e
Thanushree <thanushreemurugesan2007@gmail.com>
2026-07-27 11:48:17 +0530

    added health endpoint with congiguration

---
+++ app/api/health.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from app.schemas.health import HealthResponse
+router=APIRouter()
+
+@router.get("/health",response_model=HealthResponse)
+def health():
+    return{
+    "status":"Healthy"
+}
--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from app.core.config import settings
+from app.api.health import router as health_router 
 app=FastAPI(
-    title="helios",
-    description="Distributed LLM Gateway"
+    title=settings.APP_NAME,
+    description="Distributed LLM Gateway",
+    version=settings.VERSION
 )
-
+app.include_router(health_router)
 @app.get("/")
 def root():
     return{
-        "Project":"Helios",
+        "Project":settings.APP_NAME,
         "Status":"Running"
     }
 
