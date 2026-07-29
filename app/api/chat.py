42920aeb8a8c58d6b64eb0ff42d302342954274b
Thanushree <thanushreemurugesan2007@gmail.com>
2026-07-29 23:38:59 +0530

    added a response model

--- app/api/chat.py
+++ app/api/chat.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter
 
-from app.schemas.chat import ChatRequest
+from app.schemas.chat import ChatRequest,ChatResponse
 
 router = APIRouter()
 
 
-@router.post("/chat", response_model=ChatRequest)
+@router.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
 
-    return {
-        "prompt":request.prompt
+    if request.model=="llama3":
+        answer=f"[Llama3] You asked:{request.propmt}"
+    elif request.model=="gemini":
+        answer=f"[gemini] You asked:{request.prompt}"
+    else:
+        answer="Model not supported"
+    return{
+        "response":answer
     }
